653a449754dc5e83f836f7dca170ef8d4f0c1ca6
Temitope Olufohunsi Victor <jimlaps@outlook.com>
2019-09-21 18:36:16 +0100

    Created Test project with test cases

---
+++ SupermarketTests/SupermarketTest.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using SuperMarket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketTests
+{
+    [TestFixture]
+    public class SupermarketTest
+    {
+        [Test]
+        public void Checkout1()
+        {
+            List<string> items = new List<string> { "A" };
+
+            Checkout<List<string>> c = new Checkout<List<string>>(items);
+
+            Assert.AreEqual(50, c.Total());
+
+            items.Add("A");
+            items.Add("A");
+            items.Add("A");
+            items.Add("A");
+            items.Add("A");
+            items.Add("A");
+
+            c = new Checkout<List<string>>(items);
+            Assert.AreEqual(310, c.Total());
+
+        }
+
+        [Test]
+        public void Checkout2()
+        {
+            List<string> items = new List<string> { "B" };
+
+            Checkout<List<string>> c = new Checkout<List<string>>(items);
+
+            Assert.AreEqual(30, c.Total());
+
+            items.Add("B");
+            items.Add("B");
+            items.Add("B");
+            items.Add("B");
+            items.Add("B");
+            items.Add("B");
+
+            c = new Checkout<List<string>>(items);
+            Assert.AreEqual(165, c.Total());
+
+        }
+
+        [Test]
+        public void Checkout3()
+        {
+            List<string> items = new List<string> { "D", "A", "B", "A", "C", "B", "C", "B", "B", "A" };
+
+            Checkout<List<string>> c = new Checkout<List<string>>(items);
+            Assert.AreEqual(275, c.Total());
+
+        }
+    }
+}
